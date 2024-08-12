a4894b02b77ef16c36e58f8532eaa4cd4a0c8f01
Ankit Pandey <ankit@Ankits-MacBook-Air.local>
2024-08-12 13:16:51 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+import time
+def fetch_poster(movie_id):
+    response = requests.get("http://api.themoviedb.org/3/movie/{}?api_key=9fe290105a90e875898bebb187c82dbb&language=en-US".format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/original/" + data['poster_path']
+# Recommend function
+def recommend(movie):
+    movie_index = movies_list[movies_list['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_poster = []
+    for i in movie_indices:
+        movie_id = movies_list.iloc[i[0]].movie_id
+        recommended_movies.append(movies_list.iloc[i[0]].title)
+        # fetch poster from API
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
+
+
+# Load the movies data and similarity matrix
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies_list = pd.DataFrame(movies_list)
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Streamlit app
+st.title("Movies Recommender System")
+selected_movie_name = st.selectbox('Select a movie', movies_list['title'].values)
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie_name)
+    st.write("Recommended Movies:")
+    # display posters
+    columns = st.columns(5)
+
+    for i in range(5):
+
+        with columns[i]:
+            st.text(names[i])
+            st.image(posters[i])
+    st.success('Found!', icon="✅")
